d99a752227dcc8ac0f34cffb58bd9f3cc102cd6d
Juanse <jmedinapineros@gmail.com>
2025-09-06 17:32:59 -0500

    El programa cumple con los requerimientos

--- src/promedioprecios14/PromedioPrecios14.java
+++ src/promedioprecios14/PromedioPrecios14.java
@@ -3,18 +3,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package promedioprecios14;
-
 /**
- *
  * @author juanse
+ * fecha inicio: sabado 06 de septiembre de 2025
+ * Requerimientos: Un cliente compra varios productos. 
+ * Pide el número de productos y, para cada uno, su precio unitario. 
+ * Calcula y muestra el precio promedio de los productos comprados. 
+ * Debe resolverse solo con for.
  */
+import javax.swing.JOptionPane;
 public class PromedioPrecios14 {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO
+        int cantidadProductos = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de productos: "));
+        int precioTotal = 0;
+        for (int i=0; i<cantidadProductos; i++) {
+            int precioUnidad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el precio del producto " + i));
+            precioTotal = precioTotal + precioUnidad;
+        }
+        int promedio = precioTotal/cantidadProductos;
+        JOptionPane.showMessageDialog(null, "El precio promedio de los productos \n es de: " + promedio);
     }
-    
 }
